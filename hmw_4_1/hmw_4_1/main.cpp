ca5d1cd26e82d8ee401bbaa0a7bb3727fb5aa02c
Gaurav Gulati <ggulati3@gmail.com>
2018-11-03 03:17:22 -0700

    hmw_4_1 completed

--- hmw_4_1/hmw_4_1/main.cpp
+++ hmw_4_1/hmw_4_1/main.cpp
@@ -7,8 +7,43 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int recurse(string big, string small, int index){
+    if(index == big.length()-small.length()+1){
+        return -1;
+    } else if( small == big.substr(index, small.length())){
+        return index;
+    } else {
+        return recurse(big, small, ++index);
+    }
+}
 
 int main() {
+    char answer = 'y';
+    while(answer == 'y'){
+        string full;
+        string snippet;
+        string test = "sip";
+        cout << "Enter a string: ";
+        getline(cin, full);
+        cout << "Enter a string to search for: ";
+        getline(cin, snippet);
+        int value = recurse(full, snippet, 0);
+        if(value == -1){
+            cout << "Not found!" << endl;
+        } else {
+            cout << "The position = " << value << endl;
+        }
+        
+        cout << "Continue (y/n) ";
+        cin >> answer;
+        cout << endl;
+        cin.ignore(256,'\n');
+    }
 
+    
     return 0;
 }
