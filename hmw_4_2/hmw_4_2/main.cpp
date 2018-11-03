02775681b73833caac05a3db59a77eb7eb695124
Gaurav Gulati <ggulati3@gmail.com>
2018-11-03 03:19:30 -0700

    Enviornment for hmw_4_2 setup

---
+++ hmw_4_2/hmw_4_2/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  hmw_4_2
+//
+//  Created by Gaurav Gulati on 11/3/18.
+//  Copyright © 2018 Gaurav Gulati. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
